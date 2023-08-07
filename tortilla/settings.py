0907d2c55977aa593e2001e043d86ba86962605d
Joel Perca <joelperca.vc@gmail.com>
2023-08-07 13:35:50 -0500

    Django 4.0

--- tortilla/settings.py
+++ tortilla/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'tortilla.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
     )
 }
